3ae539771c9091ddeb1289e70a262b50f489cf8e
LuisAlonsoAndradeLopez <caffeinated340@gmail.com>
2024-03-19 00:15:02 -0600

    Use Cases 05, 07 and 08 GUIS done

---
+++ ItalianPizza/XAMLViews/GUI_Inventory.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Windows;
+
+namespace ItalianPizza.XAMLViews
+{
+    /// <summary>
+    /// Lógica de interacción para GUI_Inventory.xaml
+    /// </summary>
+    public partial class GUI_Inventory : Window
+    {
+        public GUI_Inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void UsersButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OrdersButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FinanceButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProvidersButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseSessionButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddArticleButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GenerateInventoryReportOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ArticleButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectImageButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ModifyArticleButtonOnClick1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultArticleRecipeButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DisableArticleButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ModifyArticleButtonOnClick2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
