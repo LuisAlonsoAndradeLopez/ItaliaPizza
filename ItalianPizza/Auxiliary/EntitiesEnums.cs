b282e0c220aa1e67f069ad94d5642f8080e3fbeb
Luis Alonso Andrade López <118196253+LuisAlonsoAndradeLopez@users.noreply.github.com>
2024-03-25 14:01:08 -0600

    Add files via upload

---
+++ ItalianPizza/Auxiliary/EntitiesEnums.cs
@@ -0,0 +1,14 @@
+﻿namespace ItalianPizza.Auxiliary
+{
+    public enum ArticleStatus
+    {
+        Activo,
+        Inactivo
+    }
+
+    public enum ArticleTypes
+    {
+        Insumo,
+        Producto
+    }
+}
---
+++ ItalianPizza/Auxiliary/ImageManager.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace ItalianPizza.Auxiliary
+{
+    public class ImageManager
+    {
+        public byte[] GetBitmapImageBytes(BitmapImage imageSource)
+        {        
+            if (imageSource != null)
+            {
+                BitmapSource bitmapSource = imageSource as BitmapSource;
+
+                // Convert BitmapSource to Bitmap
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                MemoryStream memoryStream = new MemoryStream();
+                encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+                encoder.Save(memoryStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(memoryStream);
+
+                // Get the bytes of the Bitmap
+                byte[] bytes;
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    bytes = stream.ToArray();
+                }
+
+                return bytes;
+            }
+
+            return null;
+        }
+
+        public BitmapImage GetImageByItaliaPizzaStoragedImagePath(string filePath)
+        {
+            string incompletePath = Path.GetFullPath(filePath);
+            string pathPartToDelete = "bin\\Debug\\";
+            string completePath = incompletePath.Replace(pathPartToDelete, "");
+
+            byte[] imageData = File.ReadAllBytes(completePath);
+
+            BitmapImage imageSource = new BitmapImage();
+
+            if (imageData != null)
+            {
+                imageSource.BeginInit();
+                imageSource.StreamSource = new MemoryStream(imageData);
+                imageSource.EndInit();
+            }
+
+            return imageSource;
+        }
+    }
+}
