8495a79841ab98fe98924598b22ac5825372b4a4
Alvaro-aguirre <128319431+Alvaro-aguirre@users.noreply.github.com>
2024-04-03 01:05:33 -0600

    Funcionalidades terminadas de los CU

--- UnitTesting/UserDAOTest.cs
+++ UnitTesting/UserDAOTest.cs
@@ -1,6 +1,10 @@
 ﻿using ItalianPizza.DatabaseModel.DataAccessObject;
 using ItalianPizza.DatabaseModel.DatabaseMapping;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace UnitTesting
 {
@@ -24,6 +28,15 @@ namespace UnitTesting
             userDAO = new UserDAO();
             employeeToRegist = new EmployeeSet()
             {
+                Names = "Test",
+                LastName = "Test",
+                SecondLastName = "Test",
+                Email = "test@gmail.com",
+                Phone = "1234567890",
+                UserStatusId = validValue,
+                EmployeePositionId = validValue,
+                Address_Id = validValue,
+
             };
             employeeAlreadyRegistered = new EmployeeSet()
             {
@@ -33,6 +46,8 @@ namespace UnitTesting
             };
             accountToRegist = new UserAccountSet()
             {
+                UserName = "Test",
+                Password = "Test",
             };
             accountAlreadyRegistered = new UserAccountSet()
             {
@@ -45,10 +60,53 @@ namespace UnitTesting
         [TestCleanup]
         public void TestCleanup()
         {
-            using(var scope = new TransactionScope())
+            using (var scope = new TransactionScope())
             {
                 scope.Complete();
             }
         }
+
+        [TestMethod]
+        public void RegisterUserTest()
+        {
+            using (var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadUncommitted }))
+            {
+                int result = userDAO.RegisterUser(accountToRegist, employeeToRegist);
+                Assert.AreEqual(2, result);
+            }
+        }
+
+        /* Falta hacer bien esta prueba
+        [TestMethod]
+        public void RegisterUserTest_InvalidByUserAlreadyRegistered()
+        {
+            using (var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadUncommitted }))
+            {
+                int result = userDAO.RegisterUser(accountAlreadyRegistered, employeeAlreadyRegistered);
+                Assert.AreEqual(0, result);
+            }
+        }
+        */
+
+        [TestMethod]
+        public void GetAllEmployeesTest()
+        {
+            using (var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadUncommitted }))
+            {
+                List<EmployeeSet> employees = userDAO.GetAllEmployees();
+                Assert.IsTrue(employees.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void GetUserAddressTest()
+        {
+            using (var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadUncommitted }))
+            {
+                String address = userDAO.GetUserAddressByEmployeeID(1);
+                Assert.IsNotNull(address);
+            }
+        }
+
     }
 }
