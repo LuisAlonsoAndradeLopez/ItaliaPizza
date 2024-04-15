30082f2d87effef2c07010bc26139f426adb977e
Miguel Camo <camomiguel1@gmail.com>
2024-04-15 00:41:07 -0600

    Formulario Registrar Proveedor

---
+++ ItalianPizza/XAMLViews/Suppliers/SuppliersForm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItalianPizza.XAMLViews.Suppliers
+{
+    /// <summary>
+    /// Lógica de interacción para SuppliersForm.xaml
+    /// </summary>
+    public partial class SuppliersForm : UserControl
+    {
+        public SuppliersForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
